876d18592091d2a18f9116edee432c3721317282
gurjindersingh <gurjindersingh663@gmail.com>
2022-05-23 23:36:01 +0530

    Header and center view added on Onboarding UI

--- OnBoardIngUI/OnBoardIngUI/Screens/OnBoardView.swift
+++ OnBoardIngUI/OnBoardIngUI/Screens/OnBoardView.swift
@@ -10,14 +10,47 @@ import SwiftUI
 struct OnBoardView: View {
     @AppStorage("onboarding") var isShowOnBoard = true
     var body: some View {
-        VStack(spacing: 40) {
-            Text("OnBoard View").font(.largeTitle)
-            Button(action:{
-                isShowOnBoard = false
-            }) {
-                Text("Go To Home")
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                VStack(spacing: 10) {
+                    // MARK: HEADER
+                    Spacer()
+                    Text("Share")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into saying
+                    """)
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                } // VStack
+                Spacer()
+                // MARK: CENTER
+                ZStack {
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 30)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 60)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: FOOTER
             }
-        }
+        } // ZStack
+        
+        
     }
 }
 
